7b3c30875f5a4ff9b9cf581f9ab73a6ba6a7fdb1
Your Name <you@example.com>
2019-08-09 22:17:16 +0200

    Dumping parameters added

--- run.py
+++ run.py
@@ -9,6 +9,8 @@ from skimage.io import imshow
 from skimage.color import rgb2grey
 from skimage.transform import resize
 from skimage.viewer import ImageViewer
+import os
+import tensorflow as tf
 
 ENV_NAME = "SpaceInvaders-v0"
 
@@ -16,8 +18,8 @@ GAMMA = 0.95
 LEARNING_RATE = 0.001
 
 MEMORY_SIZE = 1000000
-BATCH_SIZE = 64
-APPLY_STEPS = 50
+BATCH_SIZE = 31
+APPLY_STEPS = 49
 
 EXPLORATION_MAX = 1.0
 EXPLORATION_MIN = 0.01
@@ -27,6 +29,29 @@ SAVE_STEPS = 2000
 STATE_BUFFER_SIZE = 4
 INPUT_SIZE = [90,84,STATE_BUFFER_SIZE]
 NUMBER_OF_ACTIONS = 6
+GOOD_GAME_TH = 100
+BATCH_NORM = False
+
+def writeParams(path):
+    with open(path, "a") as f:
+        f.write("GAMMA : {}\n".format(GAMMA))
+        f.write("LEARNING_RATE : {}\n".format(LEARNING_RATE))
+        f.write("MEMORY_SIZE : {}\n".format(MEMORY_SIZE))
+        f.write("BATCH_SIZE : {}\n".format(BATCH_SIZE))
+        f.write("APPLY_STEPS : {}\n".format(APPLY_STEPS))
+        f.write("EXPLORATION_MAX : {}\n".format(EXPLORATION_MAX))
+        f.write("EXPLORATION_MIN : {}\n".format(EXPLORATION_MIN))
+        f.write("EXPLORATION_DECAY : {}\n".format(EXPLORATION_DECAY))
+        f.write("SAVE_STEPS : {}\n".format(SAVE_STEPS))
+        f.write("STATE_BUFFER_SIZE : {}\n".format(STATE_BUFFER_SIZE))
+        f.write("INPUT_SIZE : {}\n".format(INPUT_SIZE))
+        f.write("BATCH_NORM : {}\n".format(BATCH_NORM))
+
+path = "./batch_size={}_apply_steps={}_state_bufS={}".format(BATCH_SIZE,APPLY_STEPS,STATE_BUFFER_SIZE)
+if not os.path.exists(path):
+    print("Creating folder")
+    os.mkdir(path)
+writeParams(path + "/params.txt")
 
 def get_batch_from_memory(idxs, memory, target_network):
 
@@ -50,8 +75,8 @@ def get_batch_from_memory(idxs, memory, target_network):
 
 
 class DQNSolver:
-    def __init__(self, batch_norm = False):
-        #input to network will be (None, 84,84,4)
+    def __init__(self):
+        #input to network will be (None, 90,84,4)
         conv1 = Conv2D(16,8, strides = (4,4), activation = Activation("relu"))
         bn1 = BatchNormalization()
         conv2 = Conv2D(32,4, strides = (2,2), activation = Activation("relu"))
@@ -63,14 +88,14 @@ class DQNSolver:
 
         model = Sequential()
         model.add(conv1)
-        if(batch_norm):
+        if(BATCH_NORM):
             model.add(bn1)
         model.add(conv2)
-        if(batch_norm):
+        if(BATCH_NORM):
             model.add(bn2)
         model.add(flat_feature)
         model.add(fc1)
-        if(batch_norm):
+        if(BATCH_NORM):
             model.add(bn3)
         model.add(outputs)
 
@@ -95,10 +120,11 @@ class DQNSolver:
         if random.random() < epsilon:
             return np.random.randint(0,NUMBER_OF_ACTIONS)
         else:
-            return np.argmax(self.predict(state))
-
+            return np.argmax(self.predict(np.expand_dims(state,0)))
+    def load_weights(self, path):
+        self.model.load_weights(path)
     def save_weights(self, step):
-        model.save_weights('model_weights_{}.h5'.format(step))
+        self.model.save_weights(path + '/model_weights_{}.h5'.format(step))
 
 def processState(state, stateBuffer):
     state = transformState(state)
@@ -114,19 +140,31 @@ def transformState(state):
     offset = [12,8]
     cropped = greyResized[offset[0]:-offset[1],:]
     final = np.expand_dims(cropped,2)
-    
-    #viewer = ImageViewer(final[:,:,0])
-    #viewer.show()
+
+    #viewer = ImageViewer(np.squeeze(final[:,:],2))
+    #viewer.show()    
     return final
 
 def stateWithHistory(stateBuffer):
     concList = [stateBuffer[i] for i in range(STATE_BUFFER_SIZE-1,-1,-1)]
-    print("concList shape is {}".format(len(concList)))
+    #print("concList shape is {}".format(len(concList)))
     return np.concatenate(concList, axis = 2)
 
 
 def spaceInvaders(episodes = 1000):
+    print("Trainininininng")
     env = gym.make(ENV_NAME)
+    gameScores = []
+    goodGameScores = []
+
+    s = tf.placeholder(dtype = tf.float32)
+    gameScoreMean = tf.reduce_mean(s)
+
+    tf.reset_default_graph()
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
+    mean_summary = tf.summary.scalar(tensor = s, name = "mean")
 
     print("Action meanings : {}".format(env.get_action_meanings()))
 
@@ -151,7 +189,7 @@ def spaceInvaders(episodes = 1000):
 
         while(not terminal):
             step += 1
-            print("Step = {}\n".format(step))
+            #print("Step = {}\n".format(step))
             env.render()
             action = q_network.next_move(state, epsilon)
             state_next, reward, terminal, _ = env.step(action)
@@ -174,8 +212,19 @@ def spaceInvaders(episodes = 1000):
             epsilon = np.clip(epsilon, EXPLORATION_MIN, EXPLORATION_MAX)
 
             if step % SAVE_STEPS == 0:
+                print("{}. steps done in episode {}, saving work...".format(step,e))
                 q_network.save_weights(step)
-        
-        print("Episode {} over, total reward is {} and exploration rate is now {}".format(e, cumulative_reward, epsilon))
+        if(cumulative_reward > GOOD_GAME_TH):
+            goodGameScores.append(cumulative_reward)
+        gameScores.append(cumulative_reward)
 
-spaceInvaders(1000)
+        summary = sess.run(mean_summary)
+        writer.add_summary(summary, e)
+
+        #sess.run(gameScoreMean, feed_dict={s : gameScores})
+        
+        print("Episode {} over, total reward is {} and exploration rate is now {}. \n Mean score is {}, and filtered mean score is {}(total {} games count)".format(e, 
+            cumulative_reward, epsilon, np.mean(gameScores), np.mean(goodGameScores), len(goodGameScores)))
+
+if __name__ == "__main__":
+    spaceInvaders(1000)
